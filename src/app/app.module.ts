87a6e5414fc1491f6d5f01b73ddb063d1255b8e8
lmuelle9 <lmueller@t-systems.com>
2018-06-25 13:25:37 +0200

    Implementation about & contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { FormsModule } from '@angular/Forms';
 import { AppComponent } from './app.component';
 import { ConfigPageComponent } from './config-page/config-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ConfigPageComponent,
-    NavBarComponent
+    NavBarComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
