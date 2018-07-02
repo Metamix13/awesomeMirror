1af3b74e6a80f207473fe2516520a3e275944fdf
lmuelle9 <lmueller@t-systems.com>
2018-07-02 14:37:59 +0200

    Dashboard: x & y Position im Objekt gesichert und durch drag and drop veränderbar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FeatureService} from '../shared/feature.service';
 import {Feature} from '../shared/feature.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,9 +16,10 @@ export class DashboardComponent implements OnInit {
     this.featureService.getFeatures().subscribe(features => {
       this.localFeatureList = features;
     });
-
-
-
+  }
+  onMoveEnd(feature, event){
+    _.find(this.localFeatureList, {name: feature.name}).xPos = event.x;
+    _.find(this.localFeatureList, {name: feature.name}).yPos = event.y;
   }
 
   }
--- src/app/shared/feature.model.ts
+++ src/app/shared/feature.model.ts
@@ -2,10 +2,14 @@ export class Feature {
   public name: string;
   public description: string;
   public shown: boolean;
+  public xPos: number;
+  public yPos: number;
 
   constructor(name: string, description: string, shown: boolean){
     this.name = name;
     this.description = description;
     this.shown = shown;
+    this.xPos = 0;
+    this.yPos = 0;
   }
 }
