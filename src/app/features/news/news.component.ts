e871dcb5836267e0174780f0624ec42f287250f2
lmuelle9 <lmueller@t-systems.com>
2018-07-12 17:15:55 +0200

    UNSTABLE: Austausch zwischen Clients

--- src/app/features/news/news.component.ts
+++ src/app/features/news/news.component.ts
@@ -8,10 +8,29 @@ import {NewsService} from '../../shared/news.service';
 })
 export class NewsComponent implements OnInit {
 
+  index: number;
+  actNews: object;
+
   constructor(public newsService:NewsService) {
+    this.index = 0;
+    //setInterval(() => this.increaseInterval(),1000 * 60);
+
   }
 
   ngOnInit() {
+    //this.newsService.getCurrentNewsFromNewsAPI(this.newsService.country).subscribe(data => console.log(data[0]));
   }
+
+  increaseInterval(){
+    if(this.index >= 20){
+      this.index = 0;
+    }else{
+      this.index++;
+      this.actNews = this.newsService.news[this.index];
+
+    }
+
+  }
+
 
 }
