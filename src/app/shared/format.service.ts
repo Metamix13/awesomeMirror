edbc788152f5593f5b025fc205a6527fb410c352
Metamix <muella93@gmail.com>
2018-07-13 23:18:50 +0200

    Clock-Conf: 24h Format eingeführt

--- src/app/shared/format.service.ts
+++ src/app/shared/format.service.ts
@@ -13,10 +13,10 @@ export class FormatService{
 
   constructor(){
     this.showSecSeperate = true;
-    this.formatTime = 'hh:mm';
+    this.formatTime = 'HH:mm';
     this.formatSec = ':ss';
     this.formatDate = 'fullDate';
-    this.posTimeFormats = ['h','hh','H','HH','m','mm','s','ss','h:mm','hh:mm','hh:mm:ss'];
+    this.posTimeFormats = ['h','hh','H','HH','m','mm','s','ss','h:mm','hh:mm','hh:mm:ss','H:mm','HH:mm','HH:mm:ss'];
     this.posSecTimeFormats = [':ss',':s'];
     this.posDateFormats = ['shortDate','mediumDate','longDate','fullDate'];
     this.clockFontSize = 4.5;
