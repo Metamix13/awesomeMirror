7dbf62114595bd112d56e478d00c16e52cd79f08
Metamix <muella93@gmail.com>
2018-07-15 02:58:05 +0200

    Date: Übersetzung eingefügt

--- src/app/shared/format.service.ts
+++ src/app/shared/format.service.ts
@@ -6,6 +6,10 @@ export class FormatService{
   posSecTimeFormats: string[]; //Possible Sec Time Formats
   showSecSeperate:boolean; //Boolean to hide/show Sec Part
   formatDate: string; //Format for Date
+  formatDateMonth: string; //Format for Month in Date
+  posDateMonthFormats: string[]; //Possible Formats for Month in Date
+  formatDateDay: string; //Format for Day in Date
+  posDateDayFormats: string[]; // Possible Formats for Day in Date
   posDateFormats: string[]; //Possible Date Formats
   clockFontSize: number; //Clock Font Size
   dateFontSize: number; //Date Font Size
@@ -17,10 +21,14 @@ export class FormatService{
     this.showSecSeperate = true;
     this.formatTime = 'HH:mm';
     this.formatSec = ':ss';
-    this.formatDate = 'fullDate';
+    this.formatDate = 'M/d/yy';
+    this.formatDateMonth = 'MMMM';
+    this.formatDateDay = 'EEEE';
     this.posTimeFormats = ['h','hh','H','HH','m','mm','s','ss','h:mm','hh:mm','hh:mm:ss','H:mm','HH:mm','HH:mm:ss'];
     this.posSecTimeFormats = [':ss',':s'];
-    this.posDateFormats = ['shortDate','mediumDate','longDate','fullDate'];
+    this.posDateFormats = ['none','M/d/yy','M/d/yyyy','M.d.yy','M.d.yyyy'];
+    this.posDateMonthFormats = ['none', 'MMMM'];
+    this.posDateDayFormats = ['none', 'EEEE'];
     this.clockFontSize = 4.5;
     this.dateFontSize = 2;
     this.newsFontSize = 1;
